4424bf2c583aaec389bcd26d89da82401781f747
LucasTesche1 <lucas@yujilive.com.br>
2025-10-07 12:28:24 -0300

    start testing login screen, routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Dashboard</Link> | <Link to="/profile">Profile</Link>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/profile" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+
 }
 
 export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login(){
+
+  return (
+    <div>
+      <h1>Login screen test</h1>
+      <p>Testing glass!</p>
+    </div>
+  );
+}
+
+export default Login;
