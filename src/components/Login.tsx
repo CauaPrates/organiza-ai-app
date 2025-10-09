a6e98c78313a1acc87147df6d5a8c4f11ad47df2
LucasTesche1 <lucas@yujilive.com.br>
2025-10-09 15:55:44 -0300

    'ok'

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import loginImg from "../assets/login-gif2.gif"
+
+
 
 function Login(){
 
@@ -10,7 +11,7 @@ function Login(){
     
     <div className="login-container">
       <img
-      src={loginImg}
+      src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGw2OTBqZXB5YzJlZDBranJxb2VneTltNHIycDNqaXF1N3E3em8yZiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/r1IMdmkhUcpzy/giphy.gif"
       alt="Background Login"
       className="login-background"
       />
@@ -18,7 +19,7 @@ function Login(){
         <div className="container-responsive">
           <div className="upper-layer">
             <div className="inner-container">
-              <h1 className="text-center">Bem vindo!</h1>
+              <h1 className="text-center">Bem vindo ao OrganizaAí</h1>
               <div className="slider-container">
 
                 <div className={`slider-bg ${active === "signup" ? "right" : "left"}`}
@@ -36,15 +37,52 @@ function Login(){
                   Cadastrar
                 </p>
             
+              {/*Formulário*/}
+
               </div>
               <div className="form-container">
+                {active === "login" ? (
                 <form className="form-login">
                     <label className="label-login">E-mail</label>
-                    <input className="input-login" type="text" name="email" placeholder="seunome@gmail.com" />
+                    <input 
+                    className="input-login" 
+                    type="text" 
+                    name="email" 
+                    placeholder="seunome@gmail.com" />
                     <label className="label-login">Senha</label>
-                    <input className="input-login" type="password" name="pass" placeholder="Senha" />
+                    <input 
+                    className="input-login" 
+                    type="password" 
+                    name="pass" 
+                    placeholder="Senha" />
+
                     <button className="next-action-button">Entrar</button>                    
                 </form>
+                ) : (
+                <form className="form-login">
+                    <label className="label-login">Nome</label>
+                    <input 
+                    className="input-login" 
+                    type="text" 
+                    name="name" 
+                    placeholder="Seu nome" />
+                    <label className="label-login">E-mail</label>
+                    <input 
+                    className="input-login" 
+                    type="text" 
+                    name="email" 
+                    placeholder="seuemail@gmail.com" />
+                    <label className="label-login">Senha</label>
+                    <input 
+                    className="input-login" 
+                    type="password" 
+                    name="pass" 
+                    placeholder="Senha" />
+
+                    <button className="next-action-button">Cadastrar</button>    
+                                    
+                </form>                  
+                )}
               </div>
             </div>
 
